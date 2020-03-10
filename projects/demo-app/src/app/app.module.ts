65633c9168f428751d1b761dd3a3f151737c1231
渡部 未来 <ababup1192@gmail.com>
2020-03-10 17:59:10 +0900

    🎉First commit

---
+++ projects/demo-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyLibComponent } from 'my-lib'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyLibComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
